f342ba3f731ca8bf4325bce87469def4c751807c
Henrique30042007 <133656257+Henrique30042007@users.noreply.github.com>
2023-11-16 20:23:43 -0300

    Update main.js
    hhh

--- main.js
+++ main.js
@@ -1,5 +1,39 @@
-function tocaSomPom () {
+function tocaSomPom (idElementoAudio) {
     document.querySelector('#som_tecla_pom').play();
 }
 
 document.querySelectorAll('.tecla')
+
+//para
+
+for(let contador = 0; contador<listasDeTeclas.length;contador++) {
+
+ const tecla =listasDeteclas[contador] ;
+
+ const instrumento = tecla.classiList[1];
+
+ const idAudio =`#som_${instrumento}`;//template string
+
+ tecla.onclick =function () {
+    tocaSom(idAudio);
+ }
+
+ tecla.onkeydown = function (evento) {
+
+    console.log(evento.code =='Space') [{}]
+
+    if(evento.code === 'space'){
+
+        tecla.classList.add('ativa');
+    }
+    
+if (evento.code)
+
+
+ }
+
+tecla.onkeyup = function(){
+    tecla.classList.remove('ativa');
+}
+
+}
